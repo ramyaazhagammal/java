7c0f3933dc2077741a9734fd79f92a84940a39b2
ramyaazhagammal <43056756+ramyaazhagammal@users.noreply.github.com>
2018-09-07 14:55:45 +0530

    Add files via upload

---
+++ scanning program.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the value");
+    int a=sc.nextInt();
+    int b=3;
+    int c=a-b;
+    System.out.println("sub:" +c);
+
+  }
+}
