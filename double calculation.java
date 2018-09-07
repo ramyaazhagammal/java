18fa287e24c3c06e217a552af2254cdd55db6943
ramyaazhagammal <43056756+ramyaazhagammal@users.noreply.github.com>
2018-09-07 12:11:49 +0530

    Add files via upload

---
+++ double calculation.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    double num1 =15, num2 =10,sum,sub,multi,div;
+    sum=num1+num2;
+    sub=num1-num2;
+    multi=num1*num2;
+    div=num1/num2;
+  System.out.println("sum of the two numbers:"+sum);
+  System.out.println("subraction of the two numbers:"+sub);
+  System.out.println("multiplication of the two numbers:"+multi);
+System.out.println("division of the two numbers:"+div);
+  }
+}
