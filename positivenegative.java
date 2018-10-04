c6e2c073f0cc7500fa471b669d367661746859f4
ramyaazhagammal <43056756+ramyaazhagammal@users.noreply.github.com>
2018-10-04 12:25:25 +0530

    Add files via upload

---
+++ positivenegative.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ public class Main {
+  public static void main(String[] args)
+  {
+    int num=5;
+    if(num>5)
+    System.out.println(num+"is a positive number.");
+    else if(num<5)
+    System.out.println(num+"is a negative number.");
+    else if(num==0)
+    System.out.println(num+"is 0.");   
+  }
+}
